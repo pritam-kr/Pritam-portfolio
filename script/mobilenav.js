fd872db01f94363ee07b09e13c74047cf98de13c
pritam-kr <84632214+pritam-kr@users.noreply.github.com>
2021-05-28 19:41:46 +0530

    Working on javaScript

--- script/mobilenav.js
+++ script/mobilenav.js
@@ -1,3 +1,23 @@
+
+const desktopPages = ["CV", "Blog", "Resources", "Video", "Contact"];
+desktopPages.forEach((page) => {
+    var desktopEachPages = '';
+    desktopEachPages = `
+
+    <ul>
+        <li><a href="#" class="links">CV</a></li>
+        <li><a href="#" class="links">Blog</a></li>
+        <li><a href="#" class="links">Resources</a></li>
+        <li><a href="https://www.youtube.com/channel/UC7LfgvNkDe5LoQiwExMn0EA" target="_blank" class="links">Video</a></li>
+        <li><a href="#" class="links">Contact</a></li>
+    </ul>
+
+    `
+    document.querySelector('#page-section').innerHTML = desktopEachPages;
+})
+
+
+
 const btnBar = document.querySelector('.btn-bar');
 var mobilePage = document.querySelector('.mobile-navul');
 console.log(mobilePage)
